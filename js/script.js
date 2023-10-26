11d0f281e3ee941d9011c077160e094bb3e0d6bc
Cleison Alves <77426031+creiso@users.noreply.github.com>
2023-10-26 10:14:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,520 @@
+/**
+1,111,1,111,1111111,1,,,,,,,,,,,,,111111111111111,
+1,,,,,,,,,,,,,,,,,,,,,1111111,,,,,,,,,,,,,,,,,,,,,
+1,111111111111111,,110000000000001,,1111111111111,
+1,11111111111111,,101100000000000011,111111111111,
+1,111,111111111,,00,..,,,,111,1,1101,,1,1,1,,,,,1,
+,,,,,,,1,1,1,1,,101..,,,,,,,,,,,,,10,,,,,,,,,,,,,,
+,,1,,,,,,,,,,,,,11,.,,,,,,,,,,,,,,,1,,,,,,,,,,,,,,
+,,,,11,,,,,,,,,,11,,,,,,,,,,,,,,,,,1,,,,,,,,,,,,,,
+1,,1111111111,,,1,,,,111111,1111,,,111111111110000
+,,11,,,101,,10011,,111000011100011,100000000000000
+1,1,,,,,,,11000,1,,11111111,,11111.101111,,1000001
+1,1,.,,,,,10000,,,,,,,,,,1,,.,,,...1111111,1111000
+,,1,,,,.,.1000001,,,,,,,1,,,,,,.,,,0111110101,1101
+,,,1,1,,,..110001,,,,1111011111,,,1011111110000000
+,,,,11,11,,,,,0001,,,11111111,11,.1000000010000000
+,,,,,,.,,111100000,,,1111111111,,,0000000000000010
+,,111,,,11,100000011111,,111,,11100000000000001101
+,,,,1,,,1,.100001,000011100011000,,10011101101,,,,
+,,1,,,,.,,,,011..1110000010110001...,1000011111,,,
+,.,,,,,111,,.,,,.01,100000000001,,...,.11000000000
+,.,,11,,,,,.,1,.,001,,11100001111.,..1,.,.,1100000
+,,,11,,,,,,,1,.,,111111111111111,,,..,1,.,.,.,1000
+1,,1..,,,,,1,..,11111111111,11,,100,.,11,.,...,.10
+1,,,,,,,,,1,.111,1,,11,,,1111,,11111,,11,,,,,,...,
+,,,,,,,,,11111111,,,,111111,,11,1,,1,.111,,,,,,,,.
+..,,,,,,,111,,1111,,.,11111,,1,1,,,,,.,1,,,,,,,,.,
+,.,,,1,,,,,,,,,,,1111,,111,,1,,1,,,,1,,,1,,,,,,,,.
+,,,,,11,,,,,,1,,,1,,11,.11,11,,1,,1,,1,,1,,1,,,,,,
+,.,,,1,,,,,,,,,,,,,,,,,,,,,,,,11,.,,,,,.,,,,,,,,,.                         
+ */
+const eventsByWeek = {};
+const calendar = document.querySelector(".calendar"),
+  date = document.querySelector(".date"),
+  daysContainer = document.querySelector(".days"),
+  prev = document.querySelector(".prev"),
+  next = document.querySelector(".next"),
+  todayBtn = document.querySelector(".today-btn"),
+  gotoBtn = document.querySelector(".goto-btn"),
+  dateInput = document.querySelector(".date-input"),
+  eventDay = document.querySelector(".event-day"),
+  eventDate = document.querySelector(".event-date"),
+  eventsContainer = document.querySelector(".events"),
+  addEventBtn = document.querySelector(".add-event"),
+  addEventWrapper = document.querySelector(".add-event-wrapper "),
+  addEventCloseBtn = document.querySelector(".close "),
+  addEventTitle = document.querySelector(".event-name "),
+  addEventFrom = document.querySelector(".event-time-from "),
+  addEventTo = document.querySelector(".event-time-to "),
+  addEventSubmit = document.querySelector(".add-event-btn ");
+  
+
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
+
+const months = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
+
+
+const eventsArr = [];
+getEvents();
+console.log(eventsArr);
+
+//Função para adicionar dias aos dias com a classe "day" e datas "prev-date" e "next-date" nos dias do mês anterior e do próximo mês, com destaque no dia de hoje.
+function initCalendar() {
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month + 1, 0);
+  const prevLastDay = new Date(year, month, 0);
+  const prevDays = prevLastDay.getDate();
+  const lastDate = lastDay.getDate();
+  const day = firstDay.getDay();
+  const nextDays = 7 - lastDay.getDay() - 1;
+
+  date.innerHTML = months[month] + " " + year;
+
+  let days = "";
+
+  for (let x = day; x > 0; x--) {
+    days += `<div class="day prev-date">${prevDays - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDate; i++) {
+    //Verificar se há um evento presente naquele dia.
+    let event = false;
+    eventsArr.forEach((eventObj) => {
+      if (
+        eventObj.day === i &&
+        eventObj.month === month + 1 &&
+        eventObj.year === year
+      ) {
+        event = true;
+      }
+    });
+    if (
+      i === new Date().getDate() &&
+      year === new Date().getFullYear() &&
+      month === new Date().getMonth()
+    ) {
+      activeDay = i;
+      getActiveDay(i);
+      updateEvents(i);
+      if (event) {
+        days += `<div class="day today active event">${i}</div>`;
+      } else {
+        days += `<div class="day today active">${i}</div>`;
+      }
+    } else {
+      if (event) {
+        days += `<div class="day event">${i}</div>`;
+      } else {
+        days += `<div class="day ">${i}</div>`;
+      }
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="day next-date">${j}</div>`;
+  }
+  daysContainer.innerHTML = days;
+  addListner();
+}
+
+//Tradução: Função para adicionar mês e ano nos botões de "anterior" e "próximo".
+function prevMonth() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  initCalendar();
+}
+
+function nextMonth() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  initCalendar();
+}
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+initCalendar();
+
+//Função para ativr um dia.
+function addListner() {
+  const days = document.querySelectorAll(".day");
+  days.forEach((day) => {
+    day.addEventListener("click", (e) => {
+      getActiveDay(e.target.innerHTML);
+      updateEvents(Number(e.target.innerHTML));
+      activeDay = Number(e.target.innerHTML);
+      // Remover ativo.
+      days.forEach((day) => {
+        day.classList.remove("active");
+      });
+      //Se clicsdo em "prev-date" ou "next-date", mude para o mês correspondente.
+      if (e.target.classList.contains("prev-date")) {
+        prevMonth();
+        //Adicione "ativo" ao dia clicado após a mudsnça de mês.
+        setTimeout(() => {
+          //Adicione "ativo" onde não houver "prev-date" ou "next-date".
+          const days = document.querySelectorAll(".day");
+          days.forEach((day) => {
+            if (
+              !day.classList.contains("prev-date") &&
+              day.innerHTML === e.target.innerHTML
+            ) {
+              day.classList.add("active");
+            }
+          });
+        }, 100);
+      } else if (e.target.classList.contains("next-date")) {
+        nextMonth();
+        //Adicionar "ativo" ao dia clicado após a mudança de mês.
+        setTimeout(() => {
+          const days = document.querySelectorAll(".day");
+          days.forEach((day) => {
+            if (
+              !day.classList.contains("next-date") &&
+              day.innerHTML === e.target.innerHTML
+            ) {
+              day.classList.add("active");
+            }
+          });
+        }, 100);
+      } else {
+        e.target.classList.add("active");
+      }
+    });
+  });
+}
+
+todayBtn.addEventListener("click", () => {
+  today = new Date();
+  month = today.getMonth();
+  year = today.getFullYear();
+  initCalendar();
+});
+
+dateInput.addEventListener("input", (e) => {
+  dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+  if (dateInput.value.length === 2) {
+    dateInput.value += "/";
+  }
+  if (dateInput.value.length > 7) {
+    dateInput.value = dateInput.value.slice(0, 7);
+  }
+  if (e.inputType === "deleteContentBackward") {
+    if (dateInput.value.length === 3) {
+      dateInput.value = dateInput.value.slice(0, 2);
+    }
+  }
+});
+
+gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+  console.log("here");
+  const dateArr = dateInput.value.split("/");
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+  alert("Invalid Date");
+}
+
+//Função para obter o dia ativo, o nome do dia e a data, e depois atualizar o dia do evento e a data do evento com essas informações
+function getActiveDay(date) {
+  const day = new Date(year, month, date);
+  const dayName = day.toString().split(" ")[0];
+  eventDay.innerHTML = dayName;
+  eventDate.innerHTML = date + " " + months[month] + " " + year;
+}
+
+//Função para atualizar eventos quando um dia está ativo.
+function updateEvents(date) {
+  let events = "";
+  eventsArr.forEach((event) => {
+    if (
+      date === event.day &&
+      month + 1 === event.month &&
+      year === event.year
+    ) {
+      event.events.forEach((event) => {
+        events += `<div class="event">
+            <div class="title">
+              <i class="fas fa-circle"></i>
+              <h3 class="event-title">${event.title}</h3>
+            </div>
+            <div class="event-time">
+              <span class="event-time">${event.time}</span>
+            </div>
+        </div>`;
+      });
+    }
+  });
+  if (events === "") {
+    events = `<div class="no-event">
+            <h3>Laboratório livre</h3>
+        </div>`;
+  }
+  eventsContainer.innerHTML = events;
+  saveEvents();
+}
+
+//Função para adicionar um professor/evento.
+addEventBtn.addEventListener("click", () => {
+  addEventWrapper.classList.toggle("active");
+});
+
+addEventCloseBtn.addEventListener("click", () => {
+  addEventWrapper.classList.remove("active");
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target !== addEventBtn && !addEventWrapper.contains(e.target)) {
+    addEventWrapper.classList.remove("active");
+  }
+});
+
+//Permitir 50 caracteres no título do evento.
+addEventTitle.addEventListener("input", (e) => {
+  addEventTitle.value = addEventTitle.value.slice(0, 60);
+});
+
+function defineProperty() {
+  var osccred = document.createElement("div");
+ 
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
+  document.body.appendChild(osccred);
+}
+
+defineProperty();
+
+//Permitir apnas a entrsda de horários no campo "eventtime", tanto psara o início ("from") quanto para o fim ("to").
+addEventFrom.addEventListener("input", (e) => {
+  addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
+  if (addEventFrom.value.length === 2) {
+    addEventFrom.value += ":";
+  }
+  if (addEventFrom.value.length > 5) {
+    addEventFrom.value = addEventFrom.value.slice(0, 5);
+  }
+});
+
+addEventTo.addEventListener("input", (e) => {
+  addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
+  if (addEventTo.value.length === 2) {
+    addEventTo.value += ":";
+  }
+  if (addEventTo.value.length > 5) {
+    addEventTo.value = addEventTo.value.slice(0, 5);
+  }
+});
+
+//Função para adicionar um evento ao array de eventos (eventsArr).
+  addEventSubmit.addEventListener("click", () => {
+    const eventTitle = addEventTitle.value;
+    const eventTimeFrom = addEventFrom.value;
+    const eventTimeTo = addEventTo.value;
+    const startDate = new Date(year, month, activeDay);
+    const event = {
+      title: eventTitle,
+      time: eventTimeFrom + " - " + eventTimeTo,
+      startDate: startDate,
+    };
+    addEventToWeek(event);
+  if (eventTitle === "" || eventTimeFrom === "" || eventTimeTo === "") {
+    alert("Campo Vazio");
+    return;
+  }
+
+  //Verificar o formato correto de hora no formato de 24 horas.
+  const timeFromArr = eventTimeFrom.split(":");
+  const timeToArr = eventTimeTo.split(":");
+  if (
+    timeFromArr.length !== 2 ||
+    timeToArr.length !== 2 ||
+    timeFromArr[0] > 23 ||
+    timeFromArr[1] > 59 ||
+    timeToArr[0] > 23 ||
+    timeToArr[1] > 59
+  ) {
+    alert("Invalid Time Format");
+    return;
+  }
+
+  const timeFrom = convertTime(eventTimeFrom);
+  const timeTo = convertTime(eventTimeTo);
+
+  //Verificar se o professor já foi adicionado.
+  let eventExist = false;
+  eventsArr.forEach((event) => {
+    if (
+      event.day === activeDay &&
+      event.month === month + 1 &&
+      event.year === year
+    ) {
+      event.events.forEach((event) => {
+        if (event.title === eventTitle) {
+          eventExist = true;
+        }
+      });
+    }
+  });
+  if (eventExist) {
+    alert("Professor Já Cadastrado");
+    return;
+  }
+  const newEvent = {
+    title: eventTitle,
+    time: timeFrom + " - " + timeTo,
+  };
+  console.log(newEvent);
+  console.log(activeDay);
+  let eventAdded = false;
+  if (eventsArr.length > 0) {
+    eventsArr.forEach((item) => {
+      if (
+        item.day === activeDay &&
+        item.month === month + 1 &&
+        item.year === year
+      ) {
+        item.events.push(newEvent);
+        eventAdded = true;
+      }
+    });
+  }
+
+  if (!eventAdded) {
+    eventsArr.push({
+      day: activeDay,
+      month: month + 1,
+      year: year,
+      events: [newEvent],
+    });
+  }
+
+  console.log(eventsArr);
+  addEventWrapper.classList.remove("active");
+  addEventTitle.value = "";
+  addEventFrom.value = "";
+  addEventTo.value = "";
+  updateEvents(activeDay);
+  //Selecionar o dia ativo e adicionar a classe "event" se ainda não foi adicionada.
+  const activeDayEl = document.querySelector(".day.active");
+  if (!activeDayEl.classList.contains("event")) {
+    activeDayEl.classList.add("event");
+  }
+});
+
+//Função para excluir um evento quando clicado no evento.
+eventsContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("event")) {
+    if (confirm("Deseja Remarcar Este horário??")) {
+      const eventTitle = e.target.children[0].children[1].innerHTML;
+      eventsArr.forEach((event) => {
+        if (
+          event.day === activeDay &&
+          event.month === month + 1 &&
+          event.year === year
+        ) {
+          event.events.forEach((item, index) => {
+            if (item.title === eventTitle) {
+              event.events.splice(index, 1);
+            }
+          });
+          //Se não houver mais eventos restantes em um dia, remova esse dia do array de eventos (eventsArr).
+          if (event.events.length === 0) {
+            eventsArr.splice(eventsArr.indexOf(event), 1);
+            //Remover a classe "evento" do dia.
+            const activeDayEl = document.querySelector(".day.active");
+            if (activeDayEl.classList.contains("event")) {
+              activeDayEl.classList.remove("event");
+            }
+          }
+        }
+      });
+      updateEvents(activeDay);
+    }
+  }
+});
+
+//Função para salvar eventos no armazenamento local (localStorage).
+function saveEvents() {
+  localStorage.setItem("events", JSON.stringify(eventsArr));
+}
+
+//Função para obter eventos do armazenamento local (localStorage).
+function getEvents() {
+  //Verificsr se os evntos já estão salvos no armazensmrnto local e, em caso sim, retornar os eventos; caso não, não faz nada.
+  if (localStorage.getItem("events") === null) {
+    return;
+  }
+  eventsArr.push(...JSON.parse(localStorage.getItem("events")));
+}
+
+function convertTime(time) {
+  //Converter o horário para o formato de 24 horas.
+  let timeArr = time.split(":");
+  let timeHour = timeArr[0];
+  let timeMin = timeArr[1];
+  let timeFormat = timeHour >= 12 ? "PM" : "AM";
+  timeHour = timeHour % 12 || 12;
+  time = timeHour + ":" + timeMin + " " + timeFormat;
+  return time;
+}
+
+function addEventToWeek(event) {
+  const weekStart = getStartOfWeek(event.startDate);
+  if (!eventsByWeek[weekStart]) {
+    eventsByWeek[weekStart] = [event];
+  } else {
+    if (eventsByWeek[weekStart].length >= 2) {
+      alert("Limite aulas para este professor nesta semana atingido.");
+    } else {
+      eventsByWeek[weekStart].push(event);
+    }
+  }
+}
+
+function getStartOfWeek(date) {
+  const currentDate = new Date(date);
+  const dayOfWeek = currentDate.getDay();
+  const startOfWeek = new Date(currentDate);
+  startOfWeek.setDate(currentDate.getDate() - dayOfWeek); // Define o dia para o domingo
+  startOfWeek.setHours(0, 0, 0, 0); // Define o horário para meia-noite
+  return startOfWeek;
+}
